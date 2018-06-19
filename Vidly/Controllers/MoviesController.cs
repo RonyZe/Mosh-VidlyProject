d8f5215750ce18fed88aef461573174bef3b0c58
Rehan <zrony58@gmail.com>
2018-06-19 19:10:08 +0500

    Files Add

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+        //        public ActionResult Edit(int id)
+        //        {
+        //            return Content("id=" + id);
+        //        }
+        //
+        //        //movies
+        //        public ActionResult Index(int? pageIndex, string sortBy)
+        //        {
+        //            if (!pageIndex.HasValue)
+        //                pageIndex = 1;
+        //            if (string.IsNullOrWhiteSpace(sortBy))
+        //                sortBy = "Name";
+        //            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //        }
+    }
+}
